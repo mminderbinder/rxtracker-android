0e7305d693eea1a3f5c1a8b387a576b8fa60c447
mminderbinder <92769832+mminderbinder@users.noreply.github.com>
2026-03-08 14:11:41 -0400

    Updated the app's color palette.

--- app/src/main/java/com/example/rxtracker/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/rxtracker/ui/home/HomeScreen.kt
@@ -1,13 +1,10 @@
 package com.example.rxtracker.ui.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -24,9 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.composables.icons.lucide.CalendarCheck
 import com.composables.icons.lucide.Lucide
@@ -55,7 +50,7 @@ fun HomeScreen() {
     )
 
     val composition by rememberLottieComposition(
-        LottieCompositionSpec.RawRes(R.raw.medicine)
+        LottieCompositionSpec.RawRes(R.raw.medication)
     )
 
     val visibleWeek = rememberFirstVisibleWeekAfterScroll(state)
